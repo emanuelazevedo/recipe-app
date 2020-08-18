833349b52ebb498a3d291d8b2afdd74395bb0be4
manu <manu@pop-os.localdomain>
2020-08-18 01:00:07 +0100

    add of api endpoint to code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Recipe from './components/Recipe'
 import Header from './components/layout/Header'
 import SearchBar from './components/SearchBar'
+import Axios from 'axios';
 
 class App extends React.Component {
 
@@ -15,7 +16,18 @@ class App extends React.Component {
         console.log(this.state.ingredients);
     }
 
-    
+    searchRecipe = event => {
+        event.preventDefault();
+        
+        const ingredients = {
+          ingredients: this.state.ingredients
+        }
+        console.log(ingredients);
+        Axios.post(`https://api.spoonacular.com/recipes/findByIngredients/information?ingredients=apples,+flour,+sugar&number=2&apiKey=40fc2345992540f0be8fd7b73b307777`)
+          .then(res => console.log(res))
+    }
+
+
 
 
     render() {
@@ -23,6 +35,9 @@ class App extends React.Component {
             <div className="App">
               <Header/>
               <SearchBar ingredients= {this.addIngredients} />
+              <form onSubmit={this.searchRecipe}>
+                    <input type="submit" value="Search Recipe"/>
+                </form>
               <Recipe />
             </div>
         )
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 export class Recipe extends Component {
+
+    
+
     render() {
         return (
             <div>
-                <form>
-                    <input value="Search Recipe"/>
-                </form>
+                
             </div>
         )
     }
