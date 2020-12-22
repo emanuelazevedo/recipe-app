3d91210170dfd19230155df23100c118a1063936
manu <emanuel.azevedo@ua.pt>
2020-12-22 18:03:59 +0000

    Updates

---
+++ precache-manifest.37fa32002121602b3a271b83ed573ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552845e9469c0ea0902488e81c671f75",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "68fe17be631d0678afda",
+    "url": "/recipe-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "88dbdcf2b56443c023d4",
+    "url": "/recipe-app/static/js/2.ed975a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.ed975a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fe17be631d0678afda",
+    "url": "/recipe-app/static/js/main.f01d9d26.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
